884aeb942e093e3464172eaf558d47743803e292
SabaShagidzeMain <sabadoeswebdev@gmail.com>
2025-05-12 00:14:36 +0400

    style: More Responsiveness

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./style.css";
 
 const Footer = () => {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <p className="footer-text">© 2025 Structura. All Rights Reserved.</p>
+    </div>
+  );
 };
 
 export default Footer;
