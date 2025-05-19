c79190e51d051adff85e22b580ab54f57d76deaf
SabaShagidzeMain <sabadoeswebdev@gmail.com>
2025-05-19 04:02:44 +0400

    feat: User Interface

--- src/Components/MainPage/MainPage.jsx
+++ src/Components/MainPage/MainPage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import LandingTop from "../LandingTop/LandingTop";
 
 const MainPage = () => {
-  return <div>MainPage</div>;
+  return (
+    <div className="main-wrapper">
+      <LandingTop />
+    </div>
+  );
 };
 
 export default MainPage;
