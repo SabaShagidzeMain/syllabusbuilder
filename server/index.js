65e836bcc1be3dfe78ea6eafa94641899b9fd27b
SabaShagidzeMain <sabadoeswebdev@gmail.com>
2025-08-07 02:09:44 +0400

    fix: multiple minor/mid size bugs

--- server/index.js
+++ server/index.js
@@ -1,19 +1,19 @@
-import express from "express";
+import e from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { registerProfessor } from "./registerProfessor.js";
 
 dotenv.config();
 
-const app = express();
+const app = e();
 app.use(cors());
-app.use(express.json());
+app.use(e.json());
 
 app.post("/api/registerProfessor", registerProfessor);
 
 const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
-    res.send("Server is running!");
+  res.send("Server is running!");
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
